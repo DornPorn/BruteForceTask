980f50883bfbc96040b7111a5174b3608efd37a7
DornPorn <gungan722121@gmail.com>
2022-07-23 15:20:33 +0900

    - First task
    - Random password generated
    - UI actions are not blocked

---
+++ Pr2503/String+Ext.swift
@@ -0,0 +1,34 @@
+//
+//  String+Ext.swift
+//  Pr2503
+//
+//  Created by Stanislav Rassolenko on 7/23/22.
+//
+
+import Foundation
+
+extension String {
+    var digits:      String { return "0123456789" }
+    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
+    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
+    var letters:     String { return lowercase + uppercase }
+    var printable:   String { return digits + letters + punctuation }
+
+
+
+    mutating func replace(at index: Int, with character: Character) {
+        var stringArray = Array(self)
+        stringArray[index] = character
+        self = String(stringArray)
+    }
+}
+
+func indexOf(character: Character, _ array: [String]) -> Int {
+    return array.firstIndex(of: String(character))!
+}
+
+func characterAt(index: Int, _ array: [String]) -> Character {
+    return index < array.count ? Character(array[index])
+                               : Character("")
+}
--- Pr2503/ViewController.swift
+++ Pr2503/ViewController.swift
@@ -1,7 +1,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
     @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var bruteForceActiviryIndicator: UIActivityIndicatorView!
+    
+    @IBOutlet weak var hackedPasswordLabel: UILabel!
+    
+    private let handler = BruteForceHandler()
     
     var isBlack: Bool = false {
         didSet {
@@ -19,73 +28,48 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        self.bruteForce(passwordToUnlock: "1!gr")
-        
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        var pw = ""
+        DispatchQueue.main.async {
+            self.resetUI()
+            self.bruteForceActiviryIndicator.startAnimating()
+            pw = self.handler.generateRandomPasswordTounlock(length: 4)
+            self.passwordTextField.text = pw
+            
+            DispatchQueue.global().async {
+                self.bruteForce(passwordToUnlock: pw)
+            }
+        }
     }
     
     func bruteForce(passwordToUnlock: String) {
         let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
-
         var password: String = ""
-
         // Will strangely ends at 0000 instead of ~~~
         while password != passwordToUnlock { // Increase MAXIMUM_PASSWORD_SIZE value for more
-            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
-//             Your stuff here
-            print(password)
-            // Your stuff here
+            password = handler.generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
         }
         
-        print(password)
-    }
-}
-
-
-
-extension String {
-    var digits:      String { return "0123456789" }
-    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
-    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
-    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
-    var letters:     String { return lowercase + uppercase }
-    var printable:   String { return digits + letters + punctuation }
-
-
-
-    mutating func replace(at index: Int, with character: Character) {
-        var stringArray = Array(self)
-        stringArray[index] = character
-        self = String(stringArray)
-    }
-}
-
-func indexOf(character: Character, _ array: [String]) -> Int {
-    return array.firstIndex(of: String(character))!
-}
-
-func characterAt(index: Int, _ array: [String]) -> Character {
-    return index < array.count ? Character(array[index])
-                               : Character("")
-}
-
-func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
-    var str: String = string
-
-    if str.count <= 0 {
-        str.append(characterAt(index: 0, array))
-    }
-    else {
-        str.replace(at: str.count - 1,
-                    with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
-
-        if indexOf(character: str.last!, array) == 0 {
-            str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+        DispatchQueue.main.async {
+            self.hackedPasswordLabel.text = password
+            self.passwordTextField.isSecureTextEntry = false
+            self.bruteForceActiviryIndicator.stopAnimating()
+            self.bruteForceActiviryIndicator.isHidden = true
         }
     }
-
-    return str
+    
+    func resetUI() {
+        self.passwordTextField.isSecureTextEntry = true
+        self.hackedPasswordLabel.text = "Hacked password"
+        self.bruteForceActiviryIndicator.isHidden = false
+    }
 }
+
+
+
+
+
+
 
